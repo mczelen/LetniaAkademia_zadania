827a88b9309534d1a8af84bf6146f7c11b8cc1b6
mczelen <mateusz.czelen@gmail.com>
2022-05-21 12:28:14 +0200

    zad1: apply promotion

--- zad1/customer.py
+++ zad1/customer.py
@@ -1,12 +1,22 @@
 from datetime import date
 
 class Customer:
-    def __init__(self, first_name:str, last_name:str, pesel:str, birth_date:str):
+    def __init__(self, first_name:str, last_name:str, pesel:str, address:str, birth_date:str):
         self.first_name = first_name
         self.last_name = last_name
         self.pesel = pesel
+        self.address = address
         self.birth_date = date.fromisoformat(birth_date)
-        self.promotion_covered = False
+        self.promotion_applied = False
 
     def __str__(self):
-        return f'Customer("{self.first_name}", "{self.last_name}", "{self.pesel}", "{self.birth_date.isoformat()}")'
+        return f'Customer("{self.first_name}", "{self.last_name}", "{self.pesel}", "{self.address}", "{self.birth_date.isoformat()}", "{self.promotion_applied}")'
+
+    # Sets promotion_applied to True if customer's age is in <18,26> range 
+    def apply_promotion(self):
+        today = date.today()
+        age = today.year - self.birth_date.year # year difference between today's date and customer birth_date
+        if ((today.month, today.day) < (self.birth_date.month, self.birth_date.day)):
+            age -= 1 # subtract one if customer doesn't have birthday in current year
+        # print("age:", age)
+        self.promotion_applied = 18 <= age <= 26
--- zad1/main.py
+++ zad1/main.py
@@ -1,12 +1,19 @@
 from customer import Customer
 
 customers = [
-    Customer("Thomas", "Calderon", "99010111229", "1999-01-01"),
-    Customer("Derek", "Jones", "99010111229", "1999-01-01"),
-    Customer("Justin", "Cunningham", "99010111229", "1999-01-01"),
-    Customer("Brian", "Hernandez", "99010111229", "1999-01-01"),
-    Customer("Bobby", "Taylor", "99010111229", "2010-01-01")
-    ]
+    Customer("Thomas", "Calderon", "09291112345", "Thomas's address", "2009-09-11"),
+    Customer("Derek", "Jones", "99050712345", "Derek's address", "1999-05-07"),
+    Customer("Justin", "Cunningham", "02210112345", "Justin's address", "2002-01-01"),
+    Customer("Brian", "Hernandez", "95051012345", "Brian's address", "1995-05-10"),
+    Customer("Bobby", "Taylor", "10230112345", "Bobby's address", "2010-03-01")]
 
+print("\nBEFORE:")
+for n, customer in enumerate(customers):
+    print(f'{n}: {customer}')
+
+for n, customer in enumerate(customers):
+    customer.apply_promotion()
+
+print("\nAFTER:")
 for n, customer in enumerate(customers):
     print(f'{n}: {customer}')
