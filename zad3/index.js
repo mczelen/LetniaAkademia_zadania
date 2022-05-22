fb5d259a8069186728fd21357ccbec350e6332df
mczelen <mateusz.czelen@gmail.com>
2022-05-22 16:55:06 +0200

    zad3: tile disappear button

---
+++ zad3/index.js
@@ -0,0 +1,22 @@
+function createBtn(div) {
+    let btn = document.createElement("button");
+    btn.innerHTML = '<i class="fa fa-trash-o"></i>';
+    btn.type = "button";
+    btn.classList.add("btn", "btn-outline-danger");
+    btn.onclick = function () {
+        div.classList.add("disappeared");
+        alert("usunięto dane");
+      };
+    return btn;
+  }
+
+
+let dataContainer = document.querySelector("#data-container")
+
+for(let n = 0; n<3*4; n++){
+    let div = document.createElement("div");
+    div.innerHTML = "<p>Dane " + (n+1) + "</p>";
+    div.appendChild(createBtn(div));
+    div.classList.add("tile");
+    dataContainer.appendChild(div);
+}
