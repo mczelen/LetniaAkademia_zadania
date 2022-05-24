6a249542e8d95ae69ab0f706b9d653f63d588f01
mczelen <mateusz.czelen@gmail.com>
2022-05-24 07:42:50 +0200

    zad3: js comments

--- zad3/index.js
+++ zad3/index.js
@@ -1,18 +1,23 @@
+// creates button and its onclick method
 function createBtn(div) {
     let btn = document.createElement("button");
+    // trash bin icon
     btn.innerHTML = '<i class="fa fa-trash-o"></i>';
     btn.type = "button";
     btn.classList.add("btn", "btn-outline-danger", "btn-lg");
+    // when button is clicked call this method
     btn.onclick = function () {
         alert("usunięto dane");
+        // add "disappeared" class to parent div which makes it invisible
         div.classList.add("disappeared");
     };
     return btn;
   }
 
-
+// get reference to container for data/tile elements
 let dataContainer = document.querySelector("#data-container")
 
+// create 3*4 tiles with data and remove button
 for(let n = 0; n<3*4; n++){
     let div = document.createElement("div");
     div.innerHTML = "<p>Dane " + (n+1) + "</p>";
